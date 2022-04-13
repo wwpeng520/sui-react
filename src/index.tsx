8b0e9da308e16d97cabddc131ce1487c54678fae
wwpeng520 <wwpeng520@gmail.com>
2022-04-13 09:19:31 +0800

    feat: modify travis.yml

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
-import './styles/index.scss';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import './styles/index.scss';
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(React.createElement(App));
-reportWebVitals();
+// const root = createRoot(document.getElementById('root') as HTMLElement);
+// root.render(React.createElement(App));
+// reportWebVitals();
 
 // 组件库的根文件
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fas } from '@fortawesome/free-solid-svg-icons';
-// library.add(fas as any);
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas as any);
 
-// export { default as Button } from './components/Button';
-// export { default as Menu } from './components/Menu';
-// export { default as AutoComplete } from './components/AutoComplete';
-// export { default as Icon } from './components/Icon';
-// export { default as Input } from './components/Input';
-// export { default as Progress } from './components/Progress';
-// // export { default as Transition } from './components/Transition';
-// export { default as Upload } from './components/Upload';
+export { default as Button } from './components/Button';
+export { default as Menu } from './components/Menu';
+export { default as AutoComplete } from './components/AutoComplete';
+export { default as Icon } from './components/Icon';
+export { default as Input } from './components/Input';
+export { default as Progress } from './components/Progress';
+export { default as Transition } from './components/Transition';
+export { default as Upload } from './components/Upload';
